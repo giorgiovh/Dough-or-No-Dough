4f1d50c0e10f9c7403fd3530bf9cb564fb8404c5
giorgiovh <jorgevonhoroch@gmail.com>
2023-04-20 13:47:43 -0400

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from './pages/home/Home';
 import { Login } from './pages/login/Login';
 import { Signup } from './pages/signup/Signup';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
