a197641e071d7e4cac14b3a5926e53c32a8d98e7
giorgiovh <jorgevonhoroch@gmail.com>
2023-04-20 16:46:56 -0400

    added conditional rendering of login, signup, and logout links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,29 @@
+import { useContext } from 'react'
 import { Link } from "react-router-dom"
 
 // styles
 import styles from './Navbar.module.css'
 
+// context
+import { AuthContext } from "../context/auth-context"
+
 export default function Navbar() {
+  const auth = useContext(AuthContext)
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
+
+        {!auth.isLoggedIn && (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        )}
+        {auth.isLoggedIn && (
+          <li><Link to="/logout">Logout</Link></li>
+        )}
       </ul>
     </nav>
   )
